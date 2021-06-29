073b1f8b8429d6945ea51b392b8b6bb0b36cdcce
salvare0024 <keithangelinedgubaton@iskolarngbayan.pup.edu.ph>
2021-06-29 19:46:25 +0800

    Added Class, Clean code

--- TIC TAC TOE/Form1.cs
+++ TIC TAC TOE/Form1.cs
@@ -12,99 +12,25 @@ namespace TIC_TAC_TOE
 {
     public partial class Form1 : Form
     {
-        bool next = true;
-        int next_c = 0;
-
+        T_Class game = new T_Class();
         public Form1()
         {
             InitializeComponent();
-            
-            
         }
-        //gusto ko gumawa ng button or function na makakapag-disable ng button 
-        //once na maclick siya kelangan right button muna bago mag left ulit
-        //button click nalang huh
-
 
         private void box_Click(object sender, EventArgs e)
         {
             Button box = (Button)sender;
-            if (next)
+            if (game.Next)
                 box.Text = "X";
             else
                 box.Text = "O";
 
-            next = !next;
+            game.Next = !game.Next;
             box.Enabled = false;
-            next_c++;
+            game.Next_c++;
 
             hindi_ligwak();
-        }
-
-        private void hindi_ligwak()
-        {
-        // MALI TO HUHU
-         bool hindi_ligwak = false;
-
-            //vertical
-
-            if ((box1.Text == box4.Text) && (box4.Text == box7.Text) && (!box1.Enabled))
-                hindi_ligwak = true;
-            else if ((box2.Text == box5.Text) && (box5.Text == box8.Text) && (!box2.Enabled))
-                hindi_ligwak = true;
-            else if ((box3.Text == box6.Text) && (box6.Text == box9.Text) && (!box3.Enabled))
-                hindi_ligwak = true;
-
-            //if (hindi_ligwak)
-            //  MessageBox.Show("Yehey!");
-
-
-            //horizontal
-            else if ((box1.Text == box2.Text) && (box2.Text == box3.Text) && (!box1.Enabled))
-                hindi_ligwak = true;
-            else if ((box4.Text == box5.Text) && (box5.Text == box6.Text) && (!box4.Enabled))
-                hindi_ligwak = true;
-            else if ((box7.Text == box8.Text) && (box8.Text == box9.Text) && (!box7.Enabled))
-                hindi_ligwak = true;
-
-            //diagonal
-
-            else if ((box1.Text == box5.Text) && (box5.Text == box9.Text) && (!box1.Enabled))
-                hindi_ligwak = true;
-            else if ((box3.Text == box5.Text) && (box5.Text == box7.Text) && (!box7.Enabled))
-                hindi_ligwak = true;
-            
-            if (hindi_ligwak)
-            {
-                disableBox();
-
-                String panalo = "";
-                if (next)
-                    panalo = "O";
-                else
-                    panalo = "X";
-
-                MessageBox.Show(panalo + " ang panalo!");
-            }//tapos
-            else
-            {
-                if (next_c == 9)
-                    MessageBox.Show("Tabla");
-            }
-
-        }//katapusan na ng mundo
-
-        private void disableBox()
-        {   
-            try
-            {
-                foreach (Control c in Controls)
-                {
-                    Button box = (Button)c;
-                    box.Enabled = false;
-                }
-            }
-            catch { }
         }
 
         private void ResetBtn_Click(object sender, EventArgs e)
@@ -115,10 +41,67 @@ namespace TIC_TAC_TOE
         private void ExitBtn_Click(object sender, EventArgs e)
         {
             Application.Exit();
+        }
 
+        public void hindi_ligwak()
+        {
+
+            bool hindi_ligwak = false;
+
+            //vertical
+
+            if ((box1.Text == box4.Text) && (box4.Text == box7.Text) && (!box1.Enabled))
+                hindi_ligwak = true;
+            else if ((box2.Text == box5.Text) && (box5.Text == box8.Text) && (!box2.Enabled))
+                hindi_ligwak = true;
+            else if ((box3.Text == box6.Text) && (box6.Text == box9.Text) && (!box3.Enabled))
+                hindi_ligwak = true;
+
+            //horizontal
+            if ((box1.Text == box2.Text) && (box2.Text == box3.Text) && (!box1.Enabled))
+                hindi_ligwak = true;
+            else if ((box4.Text == box5.Text) && (box5.Text == box6.Text) && (!box4.Enabled))
+                hindi_ligwak = true;
+            else if ((box7.Text == box8.Text) && (box8.Text == box9.Text) && (!box7.Enabled))
+                hindi_ligwak = true;
+
+            //diagonal
+
+            if ((box1.Text == box5.Text) && (box5.Text == box9.Text) && (!box1.Enabled))
+                hindi_ligwak = true;
+            else if ((box3.Text == box5.Text) && (box5.Text == box7.Text) && (!box7.Enabled))
+                hindi_ligwak = true;
+
+            if (hindi_ligwak)
+            {
+                disableBox();
+
+                String panalo = "";
+                if (game.Next)
+                    panalo = "O";
+                else
+                    panalo = "X";
+
+                MessageBox.Show(panalo + " ang panalo!");
+            }
+            else
+            {
+                if (game.Next_c == 9)
+                    MessageBox.Show("Tabla");
+            }
+        }
+        public void disableBox()
+        {
+            try
+            {
+                foreach (Control c in Controls)
+                {
+                    Button box = (Button)c;
+                    box.Enabled = false;
+                }
+            }
+            catch { }
         }
     }
-
-        
-    
 }
+
---
+++ TIC TAC TOE/T_Class.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TIC_TAC_TOE
+{
+    class T_Class
+    {
+        private bool next = true;
+        private int next_c = 0;
+
+        public bool Next
+        {
+            get => next;
+            set => next = value;
+        }
+        public int Next_c
+        {
+            get => next_c;
+            set => next_c = value;
+        }
+    }
+}
